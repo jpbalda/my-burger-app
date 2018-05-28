eddc61aecc19b949e96a14344ff2296330ba76b1
Juan Pablo Balda <jpbalda@hotmail.com>
2018-05-28 18:24:45 -0500

    Starting the Burger Component

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -12,7 +12,7 @@ class BurgerIngredient extends Component {
         break;
       case 'bread-top':
         ingredient = (
-          <div className={classes.BreadBottom}>
+          <div className={classes.BreadTop}>
             <div className={classes.Seeds1} />
             <div className={classes.Seeds2} />
           </div>
