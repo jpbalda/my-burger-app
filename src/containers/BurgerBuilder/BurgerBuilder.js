c91e342b5a5b6da09908422c7fc24d35c2379808
Juan Pablo Balda <jpbalda@hotmail.com>
2018-05-28 20:16:12 -0500

    Displaying and Updating the Burger Price

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,7 +6,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 const INGREDIENT_PRICES = {
   salad: 0.5,
   cheese: 0.4,
-  meet: 1.3,
+  meat: 1.3,
   bacon: 0.7
 };
 
@@ -72,6 +72,7 @@ class BurgerBuilder extends Component {
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledInfo}
+          price={this.state.totalPrice}
         />
       </Aux>
     );
